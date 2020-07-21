bcb704833da9ca3590eb7c728f034892f694dbc9
JJ Schultz <jj@jjschultz.com>
2020-07-21 12:25:41 -0700

    initial commit

--- covidtracker/urls.py
+++ covidtracker/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 
 from django.contrib import admin
 
 admin.autodiscover()
 
-import hello.views
+import covidtracker.views
 
 # To add a new path, first import the app:
 # import blog
@@ -15,7 +15,7 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
+    path("", covidtracker.views.index, name="index"),
+    path("db/", covidtracker.views.db, name="db"),
     path("admin/", admin.site.urls),
 ]
