896fe3d102676daabf13fb5b8d3aec5ebfb6b482
JJ <jj@jjschultz.com>
2020-10-10 15:08:06 -0700

    woodcrest drive stuff

--- track19/views_mapper.py
+++ track19/views_mapper.py
@@ -15,10 +15,13 @@ def _send_response(request, tmpl, ctx=None):
 	ctx["full_url"] = request.META['PATH_INFO'] + ("?" + qs if qs is not None and len(qs) > 0 else "")
 
 	if common.get(ctx, "title") is None:
-		ctx["title"] = "Mapper"
+		ctx["title"] = "Woodcrest Drive"
 
 	if common.get(ctx, "description") is None:
-		ctx["description"] = "Maps are cool"
+		ctx["description"] = "Woodcrest Dr. through time"
+
+	if common.get(ctx, "share_image") is None:
+		ctx["share_image"] = "mapper_woodcrest/" + common.get(request.GET, "img_1", "wc_1982.jpg")
 
 	r = render(request, tmpl, context=ctx)
 
